7abcecde2c584ddc29b52953e7afb2cdc11ae27c
ZakharovaSofia <happyalpaka@mail.ru>
2022-04-30 20:42:25 +0300

    Улучшены тесты, перенесены селекторы в классы login_page.py и main_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,13 +1,37 @@
+import allure
 from pages.base_page import BasePage
-from pages.locators import LoginPageLocators
+from selenium.webdriver.common.by import By
 
 
 class LoginPage(BasePage):
+
+    def __init__(self, browser, url):
+        super().__init__(browser, url)
+        self._register_form = (By.CSS_SELECTOR, '#credential-modal-form')
+        self._registration_by_email = (By.CSS_SELECTOR, '[data-testid="credential-by-email"]')
+        self._email_input = (By.CSS_SELECTOR, '[data-testid="registration-by-email-input"]')
+        self._email_password_input = (By.CSS_SELECTOR, '[data-testid="registration-by-email-password-input"]')
+        self._confirm_email_password_input = (By.CSS_SELECTOR, '[data-testid="registration-by-email-confirm-password-input"]')
+        self._registration_agreement = (By.CSS_SELECTOR, '[class="css-1t4dsq5 e1is4j843"]')
+        self._registration_button = (By.CSS_SELECTOR, '[data-testid="registration-button"]')
+        self._recaptcha_form = (By.CSS_SELECTOR, '#recaptcha-token')
+        self._confirm_registration_code = (By.CSS_SELECTOR, '[data-testid = "code-confirmation-prompt"]')
+
+    @allure.step('Регистрация пользователя')
+    def registration_user(self, email, password):
+        self.browser.find_element(*self._registration_by_email).click()
+        self.browser.find_element(*self._email_input).send_keys(email)
+        self.browser.find_element(*self._email_password_input).send_keys(password)
+        self.browser.find_element(*self._confirm_email_password_input).send_keys(password)
+        self.browser.find_element(*self._registration_agreement).click()
+        self.browser.find_element(*self._registration_button).click()
+
+    @allure.step('Проверяем отображение формы регистрации')
     def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Не отображается форма регистрации'
+        assert self.is_element_present(*self._register_form), 'Не отображается форма регистрации'
 
-    def should_be_recaptcha_form(self):
-        assert self.is_element_present(*LoginPageLocators.RECAPTCHA_FORM), 'Не отображается captcha регистрации'
-
+    @allure.step('Проверяем отображение подтверждения регистрации и captcha')
     def should_be_confirm_registration_form(self):
-        assert self.is_element_present(*LoginPageLocators.CONFIRM_REGISTRATION_CODE), 'Не отображается окно подтверждения регистрации'
+        assert any((self.is_element_present(*self._confirm_registration_code),
+                    self.is_element_present(*self._recaptcha_form))), 'Не отображается окно подтверждения регистрации/captcha'
+
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,16 @@
+import allure
+from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
-from pages.locators import LoginPageLocators
 
 
 class MainPage(BasePage):
+
+    def __init__(self, browser, url):
+        super().__init__(browser, url)
+        self._login_button = (By.CSS_SELECTOR, '[data-testid="login-header-button"]')
+
+    @allure.step('Переходим на страницу логина')
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*LoginPageLocators.LOGIN_BUTTON)
-        login_link.click()
+        self.browser.find_element(*self._login_button).click()
+        from pages.login_page import LoginPage
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
