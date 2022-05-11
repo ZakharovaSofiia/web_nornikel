d63f41379e5896e648d1e1eba87190d38c10d08b
ZakharovaSofia <happyalpaka@mail.ru>
2022-05-11 15:52:13 +0300

    Добавлены селекторы страницы услуг

--- pages/services_page.py
+++ pages/services_page.py
@@ -9,6 +9,14 @@ class ServicesPage(BasePage):
         super().__init__(browser, url)
         self._link_services = (By.CSS_SELECTOR, '[data-testid="header-link-services"]')
         self._page_services = (By.CSS_SELECTOR, '[data-testid="services-page-title"]')
+        self._category_city = (By.CSS_SELECTOR, '[data-testid="services-page-service-category-город"]')
+        self._category_other = (By.CSS_SELECTOR, '[data-testid="services-page-service-category-другое"]')
+        self._category_health = (By.CSS_SELECTOR, '[data-testid="services-page-service-category-здоровье"]')
+        self._category_career = (By.CSS_SELECTOR, '[data-testid="services-page-service-category-карьера"]')
+        self._category_education = (By.CSS_SELECTOR, '[data-testid="services-page-service-category-образование"]')
+        self._category_travel = (By.CSS_SELECTOR, '[data-testid="services-page-service-category-путешествия"]')
+        self._category_entertainment = (By.CSS_SELECTOR, '[data-testid="services-page-service-category-развлечения"]')
+        self._category_transport = (By.CSS_SELECTOR, '[data-testid="services-page-service-category-транспорт"]')
 
     @allure.step('Переход на страницу услуг')
     def go_to_the_services_page(self):
